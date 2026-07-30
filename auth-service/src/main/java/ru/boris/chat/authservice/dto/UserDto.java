93459fd859ad100f13e9c43ab477e439fbc7dff8
Boris Ashanin <ashanin-bd@it-resource.team>
2026-07-30 18:04:00 +0300

    Валидация в сервисе авторизации, убран лишний файл

--- auth-service/src/main/java/ru/boris/chat/authservice/dto/UserDto.java
+++ auth-service/src/main/java/ru/boris/chat/authservice/dto/UserDto.java
@@ -1,7 +1,14 @@
 package ru.boris.chat.authservice.dto;
 
+import ru.boris.chat.authservice.exception.ValidationException;
+
 public record UserDto(
     Long id,
-    String username, 
-    String email) {
+    String username,
+    String email)
+{
+    public UserDto {
+        if (username == null || username.isBlank()) { throw new ValidationException("Username не может быть null");
+        }
+    }
 }
--- chat-service/src/main/java/ru/boris/chat/chatservice/dto/UserDto.java
+++ chat-service/src/main/java/ru/boris/chat/chatservice/dto/UserDto.java
@@ -1,7 +1,14 @@
 package ru.boris.chat.chatservice.dto;
 
+import ru.boris.chat.chatservice.exception.ValidationException;
+
 public record UserDto(
     Long id,
     String username, 
-    String email) {
+    String email)
+{
+    public UserDto {
+        if (username == null) { throw new ValidationException("Username не может быть null");
+        };
+    }
 }
